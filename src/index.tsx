6228dbef5f28f38a4c3c6002cec61fab22bb267c
fgfl <frederick.gfl@gmail.com>
2021-06-07 10:32:14 -0700

    repro bug caused by bg transparency

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,36 @@ import { createMuiTheme, createStyles, CssBaseline, Theme, ThemeProvider, withSt
 import App from './App';
 
 const TransparentBg = withStyles((theme: Theme) => createStyles({
-  body: {
-    backgroundColor: 'transparent',
+  '@global': {
+    body: {
+      backgroundColor: 'transparent',
+    },
   },
 }))(() => null);
 
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {
+          backgroundColor: 'transparent',
+        },
+      },
+    },
+  },
+});
+
+// Can override the background colour using the above theme or use the default theme below
+
+// const = createMuiTheme();
+
+// and have the `TransparentBg` component override the background color
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme} >
       <CssBaseline />
-      <TransparentBg />
+      {/* <TransparentBg /> */}
       <App />
     </ThemeProvider>
   </React.StrictMode>,
