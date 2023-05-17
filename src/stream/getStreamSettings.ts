467939343b0ddf680b559f14ead40f06c84532d7
fgfl <frederick.gfl@gmail.com>
2023-05-16 18:57:30 -0700

    re-enable setting for testing

--- src/stream/getStreamSettings.ts
+++ src/stream/getStreamSettings.ts
@@ -71,7 +71,7 @@ export async function getStreamSettings(): Promise<overwolf.streaming.StreamSett
         include_full_size_video: true,
         notify_dropped_frames_ratio: 0.5,
         test_drop_frames_interval: 5000,
-        // enable_on_demand_split: true,
+        enable_on_demand_split: true,
         keep_game_capture_on_lost_focus: true,
         fps: 30,
         height: 1080,
